b57eb4171979859c72f98fb69b220c6063698b66
Loghan Ramassamy <loghan.r@gmail.com>
2019-11-27 15:57:15 +0100

    setup project

---
+++ src/test/java/fr/lcdlv/AppTest.java
@@ -0,0 +1,11 @@
+package fr.lcdlv;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertThat(true).isTrue();
+    }
+}
