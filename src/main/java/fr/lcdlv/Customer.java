94ad9f26e7591b88368e838a21cd01aa7519bb13
Loghan Ramassamy <loghan.r@gmail.com>
2019-11-27 17:24:17 +0100

    extract number of sugar from instruction

--- src/main/java/fr/lcdlv/Customer.java
+++ src/main/java/fr/lcdlv/Customer.java
@@ -7,8 +7,16 @@ public class Customer {
     }
 
     public String order(String instruction) {
-        if(instruction.contains("3")) return "Drink maker makes 1 tea with 3 sugars and a stick";
-        if(instruction.contains("2")) return "Drink maker makes 1 tea with 2 sugars and a stick";
-        return "Drink maker makes 1 tea with 1 sugars and a stick";
+        String nbSugar = numberOfSugar(instruction);
+      return "Drink maker makes 1 tea with " + nbSugar + " sugars and a stick";
+    }
+
+    private String numberOfSugar(String instruction) {
+        String[] arrOfStr = splitInstruction(instruction);
+        return arrOfStr[1];
+    }
+
+    private String[] splitInstruction(String instruction) {
+        return instruction.split(":");
     }
 }
