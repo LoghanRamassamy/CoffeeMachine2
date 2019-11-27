015a9b9829e52e98ff4cdad8e1352eff3f7ae689
Loghan Ramassamy <loghan.r@gmail.com>
2019-11-27 17:59:34 +0100

    Refactor test
    Refactor test

--- src/test/java/fr/lcdlv/CustomerTest.java
+++ src/test/java/fr/lcdlv/CustomerTest.java
@@ -9,6 +9,7 @@ public class CustomerTest {
     @ParameterizedTest
     @CsvSource({
             "T::, M:Drink maker makes 1 tea with no sugar and therefore no stick",
+            "T:0:0, M:Drink maker makes 1 tea with no sugar and therefore no stick",
             "H::, M:Drink maker makes 1 chocolate with no sugar and therefore no stick",
             "C::, M:Drink maker makes 1 coffee with no sugar and therefore no stick"
     })
@@ -25,15 +26,18 @@ public class CustomerTest {
 
     @ParameterizedTest
     @CsvSource({
-            "T:1, M:Drink maker makes 1 tea with 1 sugars and a stick",
-            "H:2, M:Drink maker makes 1 chocolate with 2 sugars and a stick",
-            "C:3, M:Drink maker makes 1 coffee with 3 sugars and a stick"
+            "T:1:0, M:Drink maker makes 1 tea with 1 sugars and a stick",
+            "H:2:0, M:Drink maker makes 1 chocolate with 2 sugars and a stick",
+            "C:3:0, M:Drink maker makes 1 coffee with 3 sugars and a stick"
     })
     public void customer_instruction_is_drink_with_one_sugar_should_return_drink_message_with_sugar(String instruction, String expected) {
+        // Given
         Customer customer = new Customer();
 
+        // When
         String result = customer.order(instruction);
 
+        // Then
         assertThat(result).isEqualTo(expected);
     }
 
