ef8bc9f69bd3d4314bc37cbaa1ba6def6c648a33
Loghan Ramassamy <loghan.r@gmail.com>
2019-11-27 16:51:48 +0100

    Refactor

--- src/test/java/fr/lcdlv/CustomerTest.java
+++ src/test/java/fr/lcdlv/CustomerTest.java
@@ -1,18 +1,22 @@
 package fr.lcdlv;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class AppTest {
+public class CustomerTest {
     @ParameterizedTest
     @CsvSource({"T, M:Drink maker makes 1 tea", "H, M:Drink maker makes 1 chocolate", "C, M:Drink maker makes 1 coffee"})
     public void customer_instruction_is_drink_should_return_drink_message2(Instruction instruction, String expected) {
+        // Given
         Customer customer = new Customer();
-        assertThat(customer.order(instruction)).isEqualTo(expected);
+
+        // When
+        String result = customer.order(instruction);
+
+        // Then
+        assertThat(result).isEqualTo(expected);
     }
 
 }
