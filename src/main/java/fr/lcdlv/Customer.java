c2aff577e90ca0ff4fda341bc84212826389ce44
Loghan Ramassamy <loghan.r@gmail.com>
2019-11-27 16:09:04 +0100

    Add test customer_instruction_is_T_should_return_tea

---
+++ src/main/java/fr/lcdlv/Customer.java
@@ -0,0 +1,7 @@
+package fr.lcdlv;
+
+public class Customer {
+    public String order(String instruction) {
+        return "Tea";
+    }
+}
--- src/test/java/fr/lcdlv/AppTest.java
+++ src/test/java/fr/lcdlv/AppTest.java
@@ -1,11 +1,14 @@
 package fr.lcdlv;
 
 import org.junit.jupiter.api.Test;
+
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
     @Test
-    public void shouldAnswerWithTrue() {
-        assertThat(true).isTrue();
+    public void customer_instruction_is_T_should_return_tea() {
+        Customer customer = new Customer();
+        assertThat(customer.order("T")).isEqualTo("Tea");
     }
 }
