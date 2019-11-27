0815e2101241983382ae13e802100e5dc52e3436
Loghan Ramassamy <loghan.r@gmail.com>
2019-11-27 16:19:00 +0100

    Refactor test customer_instruction_is_T_should_return_chocolate_message

--- src/main/java/fr/lcdlv/Customer.java
+++ src/main/java/fr/lcdlv/Customer.java
@@ -2,7 +2,7 @@ package fr.lcdlv;
 
 public class Customer {
     public String order(String instruction) {
-        if(instruction == "C") return "M:Drink maker makes 1 chocolate";
+        if("C".equals(instruction)) return "M:Drink maker makes 1 chocolate";
         return "M:Drink maker makes 1 tea";
     }
 }
