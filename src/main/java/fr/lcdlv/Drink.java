1f943d26a5bcf941c4075d1406580ee53c4403a8
Loghan Ramassamy <loghan.r@gmail.com>
2019-11-28 12:11:06 +0100

    Add cost for drink

--- src/main/java/fr/lcdlv/Drink.java
+++ src/main/java/fr/lcdlv/Drink.java
@@ -1,14 +1,21 @@
 package fr.lcdlv;
 
 public enum Drink {
-    T("tea"),
-    H("chocolate"),
-    C("coffee");
+    T("tea", "0.4"),
+    H("chocolate", "0.5"),
+    C("coffee", "0.6");
 
     private final String name;
+    private final String cost;
 
     Drink(String name) {
         this.name = name;
+        this.cost = "0";
+    }
+
+    Drink(String name, String cost) {
+        this.name = name;
+        this.cost = cost;
     }
 
     public static String getNameOf(String drinkType) {
